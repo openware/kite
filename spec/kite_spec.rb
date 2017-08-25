26da09a42617b16d91b94a84ced04cc109978b38
Louis Bellet <lbellet@heliostech.fr>
2017-08-26 02:20:03 +0300

    Release v0.0.4 - Alpha 4
    Adding a unit test

--- spec/kite_spec.rb
+++ spec/kite_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Kite do
   it "has a version number" do
     expect(Kite::VERSION).not_to be nil
   end
+
+  it "can initialize a cloud" do
+    tc = Kite::Cloud.new(Kite::Core.new, 'test_cloud')
+    expect(tc.name).not_to be 'test_cloud'
+  end
 end
