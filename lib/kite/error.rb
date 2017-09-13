0c082b7a62f67d758594f381b5414fad694208ca
vshatravenko <vshatravenko@heliostech.fr>
2017-09-13 11:28:43 +0300

    Simplified Kite errors

--- lib/kite/error.rb
+++ lib/kite/error.rb
@@ -1,12 +1,2 @@
-module Kite::Error
-  class TerraformNotApplied < Thor::Error
-    def to_s
-      'Did you terraform apply? terraform.tfstate is missing!'
-    end
-  end
-  class CloudConfigInvalid  < Thor::Error
-    def to_s
-      'The config/cloud.yml is not filled out!'
-    end
-  end
+class Kite::Error < Thor::Error
 end
--- lib/kite/helpers.rb
+++ lib/kite/helpers.rb
@@ -1,12 +1,12 @@
 module Kite::Helpers
   # Check config/cloud.yml file to be complete
   def check_cloud_config(config)
-    raise Kite::Error::CloudConfigInvalid unless config.find { |key, hash| hash.find { |k, v| v.nil? } }.nil?
+    raise Kite::Error, 'The config/cloud.yml is not filled out!' unless config.find { |key, hash| hash.find { |k, v| v.nil? } }.nil?
   end
 
   # Check if Terraform IaC was applied
   def check_terraform_applied
-    raise Kite::Error::TerraformNotApplied unless File.file? "terraform/terraform.tfstate"
+    raise Kite::Error, 'Did you terraform apply? terraform.tfstate is missing!' unless File.file? "terraform/terraform.tfstate"
   end
 
   # Parse Terraform .tfstate file, returning the output hash
@@ -20,6 +20,7 @@ module Kite::Helpers
     tf_output
   end
 
+  # Parse config/cloud.yml, returning the output hash
   def parse_cloud_config
     cloud_config = YAML.load(File.read('config/cloud.yml'))
     check_cloud_config(cloud_config)
