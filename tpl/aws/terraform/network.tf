56637a7bce0210e0b88541633ff77da5894b3e00
vshatravenko <vshatravenko@heliostech.fr>
2017-09-13 21:07:46 +0300

    Moved BOSH director to private subnet
    - Added NAT
    - Added custom route table for BOSH

--- tpl/aws/terraform/network.tf
+++ tpl/aws/terraform/network.tf
@@ -17,11 +17,42 @@ resource "aws_internet_gateway" "platform" {
   }
 }
 
+# Create a NAT gateway to forward the traffic for BOSH
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id = "${aws_subnet.platform.id}"
+}
+
+# Allocate an Elastic IP for NAT gateway
+resource "aws_eip" "nat" {
+}
+
 # Grant the VPC internet access on its main route table
 resource "aws_route" "internet_access" {
   route_table_id = "${aws_vpc.platform.main_route_table_id}"
   destination_cidr_block = "0.0.0.0/0"
   gateway_id = "${aws_internet_gateway.platform.id}"
+}
+
+# Create a custom route table for the private subnet
+resource "aws_route_table" "bosh_private" {
+  vpc_id = "${aws_vpc.platform.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.nat.id}"
+  }
+
+  tags {
+    Name = "bosh_private"
+    Component = "kite-platform"
+  }
+}
+
+# Associate custom route table with private subnet
+resource {
+  subnet_id      = "${aws_subnet.bosh_private.id}"
+  route_table_id = "${aws_route_table.bosh_private.id}"
 }
 
 # Create a subnet to launch our instances into
@@ -36,15 +67,15 @@ resource "aws_subnet" "platform" {
   }
 }
 
-# Create an ops_services subnet
-resource "aws_subnet" "ops_services" {
+# Create a private subnet for BOSH director
+resource "aws_subnet" "bosh_private" {
   vpc_id = "${aws_vpc.platform.id}"
   availability_zone = "${var.aws_availability_zone}"
   cidr_block = "${var.aws_ops_subnet_cidr_block}"
   map_public_ip_on_launch = false
   tags {
     Name = "${var.aws_ops_subnet_name}"
-    Component = "ops-services"
+    Component = "kite-platform"
   }
 }
 
