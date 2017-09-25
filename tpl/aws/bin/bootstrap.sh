02ff395a2c58d1838dea1d3550cfcb3dc64a29bf
vshatravenko <vshatravenko@heliostech.fr>
2017-09-25 21:25:51 +0300

    Added bootstrap and cleanup scripts
    Moved scripts to tpl/bin

---
+++ tpl/aws/bin/bootstrap.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -xe
+
+# Apply Terraform IaC
+pushd terraform
+
+terraform init
+terraform apply
+
+popd
+
+# Set up an SSH tunnel to Bastion
+. bin/setup-tunnel.sh
+
+# Render BOSH manifest and related files
+kite render manifest bosh --cloud aws
+
+# Deploy BOSH Director
+./bin/bosh-install.sh
+
+# Set the needed environment variables
+. bin/set-env.sh
+
+# Render Concourse manifest and related files
+kite render manifest concourse --cloud aws
+
+# Deploy Concourse
+./bin/concourse-deploy.sh
