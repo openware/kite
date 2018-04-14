03e9c2a61a7fe8fe248f46180bae9f0003384b2c
Camille Meulien <camille.meulien@gmail.com>
2018-04-14 10:38:47 +0200

    Fix exit status in case of error

--- lib/kite/base.rb
+++ lib/kite/base.rb
@@ -3,6 +3,10 @@ class Kite::Base < Thor
 
   include Thor::Actions
 
+  def self.exit_on_failure?
+    true
+  end
+
   def self.source_root
     File.expand_path(File.join(File.dirname(__FILE__), "../../tpl"))
   end
